08243c66721ba6e33fa59544bd6fa0e0ef4436b6
Georgi-Progger <hugaeag@gmail.com>
2022-11-26 16:16:51 +0300

    IF and For PART

--- src/chapter2/FirstProgram.java
+++ src/chapter2/FirstProgram.java
@@ -8,28 +8,7 @@ public class FirstProgram {
         num = num * 2;
         System.out.print("Знaчeниe переменной num * 2 равно ");
         System.out.println(num);
-        IfSample.Calculate();
-        ForTest.ForConstruct();
     }
 }
 
-class IfSample {
-    public static void Calculate () {
-        int x, у;
-        x = 10;
-        у = 20;
-        if(x < у) System.out . println ( "x меньше у " );
-        x = x * 2;
-        if(x == у) System.out . println ( "x теперь равно у" );
-        x = x * 2;
-        if (x > у) System . out . println ( "x теперь больше у" ) ;
-        if (x == у) System.out . println ( "вы не увидите э того");
-    }
-}
-class ForTest {
-    public static void ForConstruct() {
-        int x;
-        for (x = 0; x < 10; x++)
-            System.out.println(" Знaчeниe х: " + x);
-    }
-}
+
