b34660fee7cf827083a4e12d7a1904d461239c5d
Georgi-Progger <hugaeag@gmail.com>
2022-11-28 16:35:13 +0300

    4 PART FOR OPERATION

---
+++ src/chapter4/MathOperation.java
@@ -0,0 +1,10 @@
+package chapter4;
+
+public class MathOperation {
+    public static void main(String[] args) {
+        int x = 42;
+        double у = 42.25;
+        System.out.println(" x mod 10 = " + x % 10);
+        System.out.println("y mod 10 " + +у % 10);
+    }
+}
